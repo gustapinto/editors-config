5e6f0b0d1638fe46e7d660bf28bb76f59bbe6ec9
Gustavo Henrique Pinto <ghp2201@gmail.com>
2021-06-01 17:54:41 -0300

    Create bash_aliases.sh

---
+++ bash_aliases.sh
@@ -0,0 +1,11 @@
+# Docker aliases
+alias dcup='docker-compose up -d'
+alias dcdown='docker-compose down'
+alias dstop='docker stop $(docker ps -q)'
+alias dinit='sudo systemctl start docker && dcup'
+dcexec() {
+    docker-compose exec "$1" "$2"
+}
+
+# Git aliases
+alias adog='git log --all --decorate --oneline --graph'
